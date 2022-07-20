0a04fb30f5070b7f22bab11daaff5915969c29e2
Arthurwzy <83602926+Arthurwzy27@users.noreply.github.com>
2022-07-20 16:05:56 +0200

    Simple weather-js app without FrontEnd

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import "./styles.css";
 
+let url = "https://api.openweathermap.org/data/2.5/weather?q=";
+let api = "API_KEY HERE"; // e.g 974658676285082805
+
 function cityWeather(city) {
-  fetch(`${process.env.API_URL}${city}&units=metric&appid=${process.env.aapi}`)
+  // fetch data for the selected city
+  fetch(`${url}${city}&units=metric&appid=${api}`)
     .then((weather) => weather.json())
-    .then((data) => displayWeather(data));
+    .then((data) => console.log(data) && displayWeather(data));
 }
 
-function displayWeather(data) {}
+function displayWeather(data) {
+  const { name } = data;
+  const { description } = data.weather[0];
+  const { temp, humidity } = data.main;
+  const { speed } = data.wind;
 
-cityWeather("Barcelona");
+  console.log(
+    `Today in ${name}, the weather will be ${description} with a temperature of ${temp}°C. Humidty: ${humidity}% & wind speed of ${speed}kph.`
+  );
+  return { name, description, temp, humidity, speed };
+}
+
+cityWeather("barcelona");
